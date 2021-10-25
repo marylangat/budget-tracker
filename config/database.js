b9cb78dd3d5eb3e73735bc0c35b2a3cb6364aab0
Marymagdalene <chemtai.langat92@gmail.com>
2021-10-25 12:27:01 -0400

    database info

--- config/database.js
+++ config/database.js
@@ -1,8 +1,16 @@
 // config/database.js
 // config/database.js
-const dotenv = require("dotenv")
-dotenv.config()
-const dbName    = 'budgettracker'
+// const dotenv = require("dotenv")
+// dotenv.config()
+// const dbName    = 'budgettracker'
+// module.exports = {
+//     'url' : `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@taskmanagercluster.aqyy0.mongodb.net/${dbName}?retryWrites=true&w=majority`};
+// //mongodb+srv://Mary:<password>@taskmanagercluster.aqyy0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+// // config/database.js
+const username  = 'Mary'
+const password  = 'BZxn35BzacSsb.!'
+const dbName    = 'roulette'
+
 module.exports = {
-    'url' : `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@taskmanagercluster.aqyy0.mongodb.net/${dbName}?retryWrites=true&w=majority`};
+    'url' : `mongodb+srv://${username}:${password}@taskmanagercluster.aqyy0.mongodb.net/${dbName}?retryWrites=true&w=majority`};
 //mongodb+srv://Mary:<password>@taskmanagercluster.aqyy0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
